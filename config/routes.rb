67602f034edd7a72870b32e6d6c63f86f4a37f77
Zaid Annas <m.zaid.annas@gmail.com>
2018-07-01 16:36:36 +0500

    Default route changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#welcome'
+  root 'home#notwelcome'
   resources :makers
   resources :home, only: [:welcome, :notwelcome]
   resources :listings do
